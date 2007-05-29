4cdebc2b63720431a34f3f7b56fb566bb7256971
sergiocirasa <sergiocirasa@1b01bc9b-1c2c-0410-9f71-9794c14141d8>
2007-05-29 03:44:06 +0000

    Añadida una caperta de forma remota

---
+++ trunk/Sergio/Arbol/cPalabra.h
@@ -0,0 +1,93 @@
+
+#if !defined PALABRA_H
+#define PALABRA_H
+
+#include <iostream>
+#include <string>
+#include "ExceptionPalabra.h"
+
+#define MAX_LONG_PALABRA 3 /**Longitud maxima para que una palabra sea almacenada en cElemento*/
+#define NAME_FILE_PALABRAS "palabras.txt" /**Nombre del archivo que contiene las palabras cuya 
+											*longitud superan a MAX_LONG_PALABRA*/
+
+enum eStateElem{IS_STRING,IS_OFFSET,IS_EMPTY};/**Diferentes estados de un cElemento*/
+
+/*****************************************************************************/
+/**Elemento que se va a guardar en el arbol.*/
+class cElemento{	
+
+public:
+
+	cElemento(){
+		tipo=IS_EMPTY;
+		nroBlock=0;
+		for(int i=0 ; i < MAX_LONG_PALABRA ; i++)
+			palabra[i]='\0';	
+	}
+
+	char tipo;/**Estado de un elemento*/ 
+			  /* Tipo=0 palabra es una palabra 
+			     Tipo=1 palabra es un offset
+				 Tipo=2 palabra esta vacia 
+	           */
+	char palabra[MAX_LONG_PALABRA];	/**Palabra u offset ha almacenar*/
+	unsigned int nroBlock; /**Numero de bloque*/		
+};
+/*****************************************************************************/
+
+class cPalabra{
+
+private:
+
+	cElemento dato;  /**Elemento que se va a guardar en el arbol*/
+	std::string palabra; /**Palbra del elemento*/
+
+public:
+	/**Constructor.	  
+	  * @throw ExceptionPalabra cPalabra::ERROR_FNF no se ha encontrado el archivo.
+	  */
+	cPalabra();
+	/**
+	 * @param elem Elemento que esta asociado a la palabra
+	 */
+	cPalabra(cElemento elem);
+	/**
+	 * @param palabra Palabra a encapsular.
+	 */
+	cPalabra(std::string palabra);
+	
+	/**Almacena la palabra en disco
+	  * @throw ExceptionPalabra cPalabra::ERROR_WIS El tamano de la palabra es menor al preestablecido.
+	  * @throw ExceptionPalabra cPalabra::ERROR_FNF No se encuentra el archivo.
+	  * @throw ExceptionPalabra cPalabra::ERROR_WNF No existe la palabra.
+	*/
+	void persistirPalabra();
+
+   /**Modifica el numero de bloque
+	* @param nro Numero de bloque al que referencia
+	*/
+	void setNroBloque(int nro);
+
+	/**Elimina el objeto*/
+	void erase();
+
+	/**Metodos requerido por el arbol*/
+	cElemento getDato();		
+
+	/**Devuelve si el objeto esta vacio o no*/
+	bool isEmpty();
+
+	bool operator==(const cPalabra &pal) const;
+	bool operator<(const cPalabra &pal) const;
+	bool operator>(const cPalabra &pal) const;	
+	bool operator<=(const cPalabra &pal) const;
+	bool operator>=(const cPalabra &pal) const;	
+	
+/*------------DESPUES SACAR------------------------------*/
+	/*Muestra la palabra*/
+	friend std::ostream &operator<<(std::ostream & salida, const cPalabra & dato);	
+/*--------------------------------------------------------*/
+
+};
+
+#endif
