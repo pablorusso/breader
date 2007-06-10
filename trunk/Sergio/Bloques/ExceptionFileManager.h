ad972f42e90c7690b488933122f0c15f3701090d
sergiocirasa <sergiocirasa@1b01bc9b-1c2c-0410-9f71-9794c14141d8>
2007-06-10 06:30:35 +0000

    fichero

--- trunk/Sergio/Bloques/ExceptionFileManager.h
+++ trunk/Sergio/Bloques/ExceptionFileManager.h
@@ -5,38 +5,51 @@
 #include <exception>
 using std::exception;
 
-enum eError{FM_ERROR_FNF,FM_ERROR_NC,FM_ERROR_RES,FM_ERROR_FIND,FM_ERROR_CNF};
+enum eError{FM_ERROR_FNF,FM_ERROR_NC,FM_ERROR_RES,FM_ERROR_FIND,FM_ERROR_CNF,
+            FM_ERROR_ADD,FM_ERROR_IC,FM_ERROR_FR,FM_ERROR,FM_ERROR_EC};
 
 class ExceptionFileManager : public exception{
 private:
 	  int tipoError;
 
 public:
-	  ExceptionFileManager(int e):tipoError(e){}
+	  
+ExceptionFileManager(int e):tipoError(e){}
 
 
-	  bool operator==(const eError &error){
-		  if(tipoError==error)
-			  return true;
-		  else return false;	  
-	  }
+bool operator==(const eError &error){
+	  if(tipoError==error)
+		  return true;
+	  else return false;	  
+}
 
-	  const char *what() const throw(){
-		 switch(tipoError){
-		/*File not Found*/
-		 case FM_ERROR_FNF: return "Error no se ha encontrado el archivo.";
-		 
-		 case FM_ERROR_NC: return "Error no se ha creado ni cargado la estructura.";
+const char *what() const throw(){
+	switch(tipoError){
+	/*File not Found*/
+	case FM_ERROR_FNF: return "Error no se ha encontrado el archivo.";
+	 
+	case FM_ERROR_NC: return "Error no se ha creado ni cargado la estructura.";
 
-		 case FM_ERROR_RES: return "Error no se ha podido restructurar el archivo.";		
+	case FM_ERROR_RES: return "Error no se ha podido restructurar el archivo.";		
 
-		 case FM_ERROR_FIND: return "Error no se ha podido encontrar el registro.";
+	case FM_ERROR_FIND: return "Error no se ha podido encontrar el registro.";
 
-		 case FM_ERROR_CNF: return "Error Categoria no encontrada.";
+	case FM_ERROR_ADD: return "Error no se ha podido insertar el registro.";
 
-		  default: return "Error Inesperado";
-		}          
-      }
+	case FM_ERROR_CNF: return "Error Categoria no encontrada.";
+
+	case FM_ERROR_FR: return "Error Categoria fuera de rango.";
+		
+	case FM_ERROR_IC: return "Error ya existe una estructura cargada.";
+
+	case FM_ERROR: return "Error Inesperado";
+
+	case FM_ERROR_EC: return "Error no se ha podido borrar la categoria.";
+			 
+	default: return "Error Inesperado";
+	}
+}
+
 };
 
 #endif
--- trunk/Sergio/Bloques/cArchivo3.h
+++ trunk/Sergio/Bloques/cArchivo3.h
@@ -2,6 +2,8 @@
 #if !defined ARCHIVO3
 #define ARCHIVO3
 
+/*NOTA: ESTA CLASE SIMULA SER LA QUE IMPLEMENTO EDUARDO O SEA NO VA*/
+
 typedef struct{
 	unsigned int firstBlockTag;
 	unsigned int firstBlockEmpty;
@@ -10,13 +12,19 @@ typedef struct{
 
 
 #include <exception>
+#include <map>
 using std::exception;
 
 enum eErrorA8{ERROR_CNF};
 
+typedef std::map<unsigned int , tRegistro3> tMap;
+
+
+/*--*--*---*---*---*---*---*---*---*---*---*---*---*---*---*---*---*---*---*---*--*---*----*--*/
 class ExceptionArchivo3 : public exception{
+
 private:
-	  int tipoError;
+	  int tipoError;	  
 
 public:
 	  ExceptionArchivo3(int e):tipoError(e){}
@@ -36,39 +44,46 @@ public:
 		}          
       }
 };
-
-
-
+/*--*--*---*---*---*---*---*---*---*---*---*---*---*---*---*---*---*---*---*---*--*---*----*--*/
 
 class cArchivo3{
+
+private:
+	tMap cont;
 
 
 public:
 	
 /*-----------------------------------------------------------------------------*/
 	void addRegistro(const unsigned int idCat,const tRegistro3 &reg){
-	
-	
+		cont.insert( tMap::value_type(idCat,reg));	
 	}
 /*-----------------------------------------------------------------------------*/
 	tRegistro3 getRegistro(const unsigned int &idCat){
 		tRegistro3 reg={0,0};
-		bool encontrada=false;
 
-		if(!encontrada)
-			throw ExceptionArchivo3(ERROR_CNF);
+		tMap::iterator it = cont.find(idCat);
+	
+
+		if( it != cont.end() ) {
+			reg = (tRegistro3) it->second;			
+		}else{
+				throw ExceptionArchivo3(ERROR_CNF);
+		}
+		
 
 		return reg;
+		
 	
 	}
 /*-----------------------------------------------------------------------------*/
 	void setRegistro(const unsigned int idCat,const tRegistro3 &reg){
-	
+
+		cont[idCat]=reg;	
 	
 	}
 /*-----------------------------------------------------------------------------*/
 
 };
-
 
 #endif
